3b2d9044731945b98ac584cb3d7a60f23d6d56cf
Worachat <worachat.smn@gmail.com>
2026-02-02 11:32:15 +0700

    Add: implement icon and layout

---
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import { Sidebar } from '../components/Sidebar';
+import { Header } from '../components/Header';
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  return (
+    <div className="flex h-screen w-full bg-[#f8fafc] overflow-hidden font-sans">
+      {/* Fixed Sidebar */}
+      <Sidebar isOpen={isSidebarOpen} />
+      
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col h-screen overflow-hidden">
+        {/* Fixed Header */}
+        <Header toggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
+        
+        {/* Scrollable Page Content */}
+        <main className="flex-1 p-5 pt-2 pb-5 overflow-hidden flex flex-col">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
